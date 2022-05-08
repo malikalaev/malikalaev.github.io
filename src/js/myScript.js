e9fa66c39a9a9310116e75f3afbe09fa9c7e8fce
malikalaev <96765937+malikalaev@users.noreply.github.com>
2022-05-08 19:28:12 +0300

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
    let valHref = $(this).attr("href");
    $('html, body').animate({ scrollTop: $(valHref).offset().top - 60 + "px"});
    });
+
    $(window).scroll(() => {
       let scrollDistance = $(window).scrollTop();
       $(".section").each(function (i, el) { 
@@ -17,6 +18,7 @@ $(document).ready(function () {
          } 
       });
    });
+
    [].forEach.call(document.querySelectorAll('img[data-src]'),
    function(img) { img.setAttribute('src', img.getAttribute('data-src'));
     img.onload = function() { img.removeAttribute('data-src'); }; 
@@ -85,52 +87,70 @@ $(document).ready(function () {
 				}, 5);
 			}
 		});
-});
-$(document).ready(function() {
    $('.image-link').magnificPopup({type:'image'});
- });
+
+	$('.review_slider').slick({
+		infinite:false,
+		dots:true
+	});
+
+	// setTimeout(function() {
+	// 	$('#modalOpen').click();
+	// },15000);
+
+let siteType = $('#siteType').val(),
+   design =  $('#design').val(),
+   adaptive =  $('#adaptive').val();
+
+let choice = siteType + design + adaptive;
+// $('#price') = text(choice);
+console.log(choice);
+function getCount (choice) {
+
+if (choice == 111) {
+   alert("Цена:11000 Сроки: 3 дня");
+} 
+else if (choice == 121) {
+   alert("Цена:12000 Сроки: 7 дней");
+}
+else if (choice == 112) {
+   alert("Цена:11000 Сроки: 9 дней");
+} 
+else if (choice == 122) {
+   alert("Цена:11000 Сроки: 12 дней");
+}
+else if (choice == 211) {
+   alert("Цена:11000 Сроки: 13 дней");
+}
+else if (choice == 221) {
+   alert("Цена:11000 Сроки: 15 дней");
+} 
+else if (choice == 212) {
+   alert("Цена:11000 Сроки: 20 дней");
+}
+else if (choice == 222) {
+   alert("Цена:11000 Сроки: 30 дней");
+}   
+else {
+   alert("вы ввели что-то не то");
+}
+}
+let result = choice;
+
+getCount(result);
 
 
 
 
 
 
-// let siteType = prompt ("1 - сайт-визитка, 2 - интернет-магазин"),
-//    design = prompt ("1 - резиновый, 2 - адаптивный"),
-//    adaptive = prompt ("1 - простенький, 2 - классный");
 
-// let choice = siteType + design + adaptive;
-// function getCount (choice) {
 
-// if (choice == 111) {
-//    alert("Цена:11000 Сроки: 3 дня");
-// } 
-// else if (choice == 121) {
-//    alert("Цена:12000 Сроки: 7 дней");
-// }
-// else if (choice == 112) {
-//    alert("Цена:11000 Сроки: 9 дней");
-// } 
-// else if (choice == 122) {
-//    alert("Цена:11000 Сроки: 12 дней");
-// }
-// else if (choice == 211) {
-//    alert("Цена:11000 Сроки: 13 дней");
-// }
-// else if (choice == 221) {
-//    alert("Цена:11000 Сроки: 15 дней");
-// } 
-// else if (choice == 212) {
-//    alert("Цена:11000 Сроки: 20 дней");
-// }
-// else if (choice == 222) {
-//    alert("Цена:11000 Сроки: 30 дней");
-// }   
-// else {
-//    alert("вы ввели что-то не то");
-// }
-// }
 
-// let result = choice;
 
-// getCount(result);
+
+});
+
+
+
+
