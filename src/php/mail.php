fe7a87a0029d1733572f9056b814f3332558f5ed
malikalaev <96765937+malikalaev@users.noreply.github.com>
2022-06-10 15:57:12 +0300

    Add files via upload

--- src/php/mail.php
+++ src/php/mail.php
@@ -4,22 +4,22 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
+$name = $_POST['nameFormphp'];
+$email = $_POST['emailFormphp'];
+$comment = $_POST['comment'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'example@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'password'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'yalgloo22200@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'yalgloss22'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('example@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('exampleto@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('yalgloo22200@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('fagov65681@runchet.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -28,13 +28,13 @@ $mail->addAddress('exampleto@p33.org');     // Кому будет уходит�
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку!';
+$mail->Subject = 'у него какие-то слова';
+$mail->Body    = '' .$name . ' отправил сообщение!' . "<br>" . " Вот его почта " .$email. "<br>" . " Сказал вот это: " . "<br> ".$comment;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
-    echo 'Error';
+    return false;
 } else {
-    echo 'success';
+    return true;
 }
 ?>
