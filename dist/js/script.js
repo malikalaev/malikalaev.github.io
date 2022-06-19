76b5cd165058d0a1181740d496384fed1f31ebee
malikalaev <96765937+malikalaev@users.noreply.github.com>
2022-06-20 01:40:33 +0300

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+$(document).ready(function () {
+   $('a[href^="#"]').click(function () {
+      let valHref = $(this).attr("href");
+      $('html, body').animate({ scrollTop: $(valHref).offset().top - 60 + "px" });
+   });
+
+
+   $(window).scroll(() => {
+      let scrollDistance = $(window).scrollTop();
+      $(".section").each(function (i, el) {
+         if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+            $("nav a").each(function (i, el) {
+               if ($(el).hasClass("active")) {
+                  $(el).removeClass("active");
+               }
+            });
+            $('nav li:eq(' + i + ')').find('a').addClass("active");
+         }
+      });
+   });
+});
